2ef650d0782b98e34ec1acdf089c421fd0b1ead7
张纯彬 <zcb973@qq.com>
2018-08-10 10:56:20 +0800

    Update heap_sort.c
    give a framework for heap_sort

--- heap_sort.c
+++ heap_sort.c
@@ -1,1 +1,37 @@
+#include <stdio.h>
 
+void build_heap(int nums[], int len);
+void adjust_heap(int nums[], int len);
+void heap_sort(int nums[], int len);
+void show_array(int nums[], int len);
+
+int main()
+{
+  int nums[] = {2, 3, 1, 9, 5, 6, 8, 7, 4};
+  int len = 9;
+  heap_sort(nums, len);
+  show_array(nums, len);
+}
+
+void build_heap(int nums[], int len)
+{
+  int mid = len / 2;
+  while (mid > 0)
+  {
+  }
+}
+
+void heap_sort(int nums[], int len)
+{
+  
+}
+
+void show_array(int nums[], int len)
+{
+  int i = 0;
+  for (i = 0; i < len; ++i)
+  {
+    printf("%d\t", nums[i]);
+  }
+  printf("\n");
+}
